1b4031e05370f779533cfd269a3e1be93a6e07e2
Mohammad Asif Ullah Chowdhury <78401115+asifthewebguy@users.noreply.github.com>
2022-02-16 03:22:09 +0600

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,31 @@ import Header from "../components/Header.vue";
 </script>
 
 <style scoped>
+/* reset start */
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol, ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+/* reset end */
 :root{
   font-size: 16px;
 }
