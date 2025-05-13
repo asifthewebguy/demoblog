401c4a9a694bd844e88f058c8f0f90e9e9b94ca4
Mohammad Asif Ullah Chowdhury <git@atwg.xyz>
2025-05-14 01:05:18 +0600

    Fix API endpoint by importing serverQueryContent and add sample article

---
+++ content/articles/hello-world.md
@@ -0,0 +1,23 @@
+---
+title: Hello World
+description: This is my first blog post
+img: /img/hello-world.jpg
+slug: hello-world
+author: John Doe
+createdAt: 2023-06-01
+---
+
+# Hello World
+
+This is my first blog post using Nuxt Content module. It's a great way to write content for your website.
+
+## Features
+
+- Markdown support
+- Front matter
+- Code highlighting
+- And more!
+
+```js
+console.log('Hello World!')
+```
--- server/api/articles.js
+++ server/api/articles.js
@@ -1,8 +1,12 @@
+// Import the correct function from the content module
+import { serverQueryContent } from '#content/server'
+
 export default defineEventHandler(async (event) => {
-  const articles = await queryContent('articles')
+  // Use serverQueryContent with the event parameter
+  const articles = await serverQueryContent(event, 'articles')
     .only(['title', 'description', 'img', 'slug', 'author'])
     .sort({ createdAt: 1 })
     .find();
-  
+
   return articles;
 });
